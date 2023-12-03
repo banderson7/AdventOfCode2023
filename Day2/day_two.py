469b7a2d069b17290180c36a9177530521768397
banders <banderson7@users.noreply.github.com>
2023-12-03 16:42:57 -0600

    abstract out games

--- Day2/day_two.py
+++ Day2/day_two.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 from Shared.open_file import open_file
 
 contents = {
@@ -7,22 +9,52 @@ contents = {
 }
 
 
+@dataclass
+class CubeSet:
+    color: str
+    count: int
+
+
+@dataclass
+class Handful:
+    cube_sets: [CubeSet]
+
+
+@dataclass
+class Game:
+    count: int
+    handfuls: [Handful]
+
+    def __init__(self, input_data: str):
+        self.handfuls = []
+        game = input_data.strip("\"")
+        game_count, cubes = game.split(':')
+        self.count = int(game_count.split(' ')[1])
+        handfuls = cubes.split(";")
+        for handful in handfuls:
+            cube_sets_data = handful.split(',')
+            cube_sets = []
+            for cube_set in cube_sets_data:
+                count, color = cube_set.strip().split(" ")
+                cube_sets.append(CubeSet(color, int(count)))
+            self.handfuls.append(Handful(cube_sets))
+
+
 def day_two_part_one():
     data = open_file(2)
     valid_games = []
     for line in data:
-        game = line.strip("\"")
-        game_count, cubes = game.split(':')
-        game_number = int(game_count.split(' ')[1])
-        cube_sets = cubes.split(";")
+        game = Game(line)
         valid_game = True
-        for cube_set in cube_sets:
-            pull = cube_set.split(',')
-            for color_group in pull:
-                count, color = color_group.strip().split(" ")
-                if int(count) > contents[color]:
+        for handful in game.handfuls:
+            for cube_set in handful.cube_sets:
+                if cube_set.count > contents[cube_set.color]:
                     valid_game = False
                     continue
         if valid_game:
-            valid_games.append(game_number)
+            valid_games.append(game.count)
     print(f'Day Two Part 1 result: {sum(valid_games)}')
+
+
+def day_two_part_two():
+    pass
